cc1ca1a129ea4adfb95318beb91824ec4c1e7e7a
Noah Wang <bwang.noah@gmail.com>
2024-01-26 21:48:57 -0800

    streamlit main branch

--- src/main.py
+++ src/main.py
@@ -1,6 +1,25 @@
+import streamlit as st
+
+
+def inp():
+    '''
+        Sets session state of input to empty string
+    '''
+    st.session_state.input = st.session_state.widget
+    st.session_state.widget = ""
 
 
 def main():
+    st.title("Quarter Planner")
+    if 'input' not in st.session_state:
+        st.session_state.input = ''
+    if 'start' not in st.session_state:
+        st.session_state.start = 0
+    empty = st.empty()
+    st.text_input("Search...", key="widget", on_change=inp)
+    #  on_change continues the code after user queries again so we do not need while True loop
+    query = st.session_state.input
+    print(query)
     pass
 
 
